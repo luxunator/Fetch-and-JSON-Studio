ed16639b521278c86bf2389e6c91b92f73aac9b6
Dylan <luxuandthebox@gmail.com>
2022-07-26 14:53:50 -0600

    Change Active Color

--- script.js
+++ script.js
@@ -11,13 +11,14 @@ window.addEventListener("load", function(){
             });
 
             for (let i = 0; i < json.length; i++) {
+                let activeContent = json[i].active ? `<li style="color:green">Active: ${json[i].active}</li>` : `<li>Active: ${json[i].active}</li>`;
                 container.innerHTML +=`
                     <div class="astronaut">
                         <div class="bio">
                             <h3>${json[i].firstName} ${json[i].lastName}</h3>
                             <ul>
                                 <li>Hours in space: ${json[i].hoursInSpace}</li>
-                                <li>Active: ${json[i].active}</li>
+                                ${activeContent}
                                 <li>Skills: ${json[i].skills.join(", ")}asd</li>
                             </ul>
                         </div>
