42ace59e6af35eee7334a6d9fbe38bd270fcab43
Dylan <luxuandthebox@gmail.com>
2022-07-26 14:31:44 -0600

    Show Astronaut Info

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // TODO: add code here
+window.addEventListener("load", function(){
+    const fetchPromise = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+        fetchPromise.then( function(response) {
+        const jsonPromise = response.json();
+        jsonPromise.then( function(json) {
+            console.log(json)
+            const container = document.getElementById("container");
+            for (let i = 0; i < json.length; i++) {
+                container.innerHTML +=`
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li>Active: ${json[i].active}</li>
+                                <li>Skills: ${json[i].skills.join(", ")}asd</li>
+                            </ul>
+                        </div>
+                    <img class="avatar" src="${json[i].picture}">
+                    </div>
+                `;
+            }
+            
+        });
+    });
+});
